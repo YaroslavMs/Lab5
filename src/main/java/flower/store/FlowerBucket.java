bd41f4e7ad6a6c92fed783f584889a973295fcb3
YaroslavTk <yaroscro@gmail.com>
2022-10-23 21:57:29 +0300

    initialized list

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -3,13 +3,14 @@ package flower.store;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class FlowerBucket {
     @Getter
     @Setter
-    private List<FlowerPack> flowerPacks;
+    private List<FlowerPack> flowerPacks = new ArrayList<>();
 
     public void add(FlowerPack flowerPack) {
         flowerPacks.add(flowerPack);
