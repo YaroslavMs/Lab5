6c1a0562e120b0ac20b8b4289c867cacfa6b57fd
YaroslavTk <yaroscro@gmail.com>
2022-10-23 21:50:36 +0300

    magic ten again

--- src/test/java/FlowerBucketTest.java
+++ src/test/java/FlowerBucketTest.java
@@ -12,6 +12,7 @@ public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int MAX_QUANTITY = 1000;
     private static final int MAX_PRICE = 100;
+    private static final int TEN_FOR_TEST = 10;
 
     private FlowerBucket flowerBucket;
 
@@ -22,11 +23,10 @@ public class FlowerBucketTest {
 
     @Test
     public void testPrice() {
-        int magicTen = 10;
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
         Flower flower = new Rose();
-        flower.setPrice(magicTen);
+        flower.setPrice(TEN_FOR_TEST);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
         flowerBucket.add(flowerPack);
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
